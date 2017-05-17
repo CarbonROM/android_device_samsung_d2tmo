c1eba048c931d71ca3357ec47bf8578219ec0dc8
Joe Maples <joe@frap129.org>
2017-05-17 10:18:49 -0500

    d2tmo: Carbonize
    Signed-off-by: Joe Maples <joe@frap129.org>

--- carbon_d2tmo.mk
+++ carbon_d2tmo.mk
@@ -1,13 +1,15 @@
 $(call inherit-product, device/samsung/d2tmo/full_d2tmo.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common.mk)
+$(call inherit-product, vendor/carbon/config/gsm.mk)
+
+PRODUCT_NAME := carbon_d2tmo
+PRODUCT_DEVICE := d2tmo
+PRODUCT_MANUFACTURER := Samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2uc \
     TARGET_DEVICE=d2tmo \
     BUILD_FINGERPRINT="samsung/d2tmo/d2tmo:4.3/JSS15J/T999UVUEMJC:user/release-keys" \
     PRIVATE_BUILD_DESC="d2tmo-user 4.3 JSS15J T999UVUEMJC release-keys"
-
-PRODUCT_NAME := lineage_d2tmo
-PRODUCT_DEVICE := d2tmo
